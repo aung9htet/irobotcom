494dc082b1878eef3f4c14bf95f398702fe9f8a0
Alejo <aljiro@gmail.com>
2023-09-03 19:00:53 +0100

    Adding a robot window

--- home_page/robot_controller.js
+++ home_page/robot_controller.js
@@ -1,22 +1,32 @@
 import RobotWindow from 'https://cyberbotics.com/wwi/R2023b/RobotWindow.js';
 
-window.onload = function() {
-  window.robotWindow = new RobotWindow();
+// window.onload = function() {
+//   window.robotWindow = new RobotWindow();
 
-  window.robotWindow.receive = function(message, robot) {
-    if (message.indexOf("configure ") === 0) {
-      console.log("Configure message");
-    } else { // sensor values
-      console.log("Other message: " + message)
-    }
-  }
-  window.robotWindow.send("configure");
-}
-
-
-window.addEventListener("mouseup", onmouseup);
+//   window.robotWindow.receive = function(message, robot) {
+//     if (message.indexOf("configure ") === 0) {
+//       console.log("Configure message");
+//     } else { // sensor values
+//       console.log("Other message: " + message)
+//     }
+//   }
+//   window.robotWindow.send("configure");
+// }
 
 
-window.onmouseup = function(event) {
-    window.robotWindow.send("mouseup");
-}
+// window.addEventListener("mouseup", onmouseup);
+
+
+// window.onmouseup = function(event) {
+//   window.robotWindow = new RobotWindow();
+
+//   window.robotWindow.receive = function(message, robot) {
+//     if (message.indexOf("configure ") === 0) {
+//       console.log("Configure message");
+//     } else { // sensor values
+//       console.log("Other message: " + message)
+//     }
+//   }
+//   window.robotWindow.send("configure");
+//     window.robotWindow.send("mouseup");
+// }
